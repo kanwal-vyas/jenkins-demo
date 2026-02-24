d50b51dccc96967492ebc6519b208e8ffca9a05f
Kanwal Vyas <kanwal.a.vyas@gmail.com>
2026-02-24 23:31:10 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,10 @@
 with open("README.md", "r") as f:
     a, b = map(int, f.read().split())
 
-print("Sum is", a + b)
+result = a + b
+
+print("Sum is", result)
+
+# condition check (simulate test)
+if result != 15:
+    raise Exception("Test Failed: Incorrect Sum")
